0268cd10e00fbbdc20e373d52a7715df246b335e
Ethan Nguyen <45704850+ethanNguyenUser@users.noreply.github.com>
2023-02-02 13:02:45 -0800

    Accounted for time delay in RT for histogram

--- Lecture 6/BasicHistogram.m
+++ Lecture 6/BasicHistogram.m
@@ -2,6 +2,9 @@
 clear all;
 close all;
 
+%Time delay from monitor
+timeDelay = 59.6;
+
 %Get paths to your .csv files using cmd/terminal
 path1 = "/Users/ethan/Desktop/Code/EMC/emc-training/Lecture 2/Data/Ethan Nguyen_01_12_Gabor Reflex Wide.csv";
 path2 = "/Users/ethan/Desktop/Code/EMC/emc-training/Lecture 2/Data/Ethan Nguyen_01_12_Gabor Two Decision Covert.csv";
@@ -19,7 +22,7 @@ figure()
 hold on
 
 %Changing data type and specifying row containing RTs
-xvals = table2array(OneDec(:,4))*1000;
+xvals = table2array(OneDec(:,4))*1000 - timeDelay;
 avals = rmoutliers(xvals,'quartiles');
 
 %Plot avals in a histogram and toggle bar & edge color
@@ -38,7 +41,7 @@ set(n,'String',{namelabel, mnlabel,stdlabel});
 
 %% Plotting Two Decision
 
-xvals = table2array(TwoDec(:,4))*1000;
+xvals = table2array(TwoDec(:,4))*1000 - timeDelay;
 avals = rmoutliers(xvals,'quartiles');
 
 histogram(avals,'FaceColor', '[0.6350, 0.0780, 0.1840]', 'EdgeColor', 'w', 'BinWidth',20);
@@ -56,7 +59,7 @@ set(n,'String',{namelabel, mnlabel,stdlabel});
 
 %% Plotting Three Decision
 %Write code to plot your 3 Dec data
-xvals = table2array(ThreeDec(:,4))*1000;
+xvals = table2array(ThreeDec(:,4))*1000 - timeDelay;
 avals = rmoutliers(xvals,'quartiles');
 
 histogram(avals,'FaceColor', '#00EE00', 'EdgeColor', 'w', 'BinWidth',20);
@@ -75,7 +78,7 @@ set(n,'String',{namelabel, mnlabel,stdlabel});
 
 %%
 legend('Reflex', 'Two Decision', 'Three Decision');
-axis([150 1200 0 20])
+axis([0 600 0 15])
 %[ymin ymax xmin xmax]
 
 xlabel("Reaction Time (ms)");
