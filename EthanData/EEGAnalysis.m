6a886c3d0e311ae844fbc914b5f8003401665348
Ethan Nguyen <45704850+ethanNguyenUser@users.noreply.github.com>
2023-02-14 15:23:00 -0800

    Updated my Red-blue stimuli experiment Matlab code

--- EthanData/EEGAnalysis.m
+++ EthanData/EEGAnalysis.m
@@ -3,86 +3,25 @@ clear all;
 close all;
 
 %Time delay from monitor
-timeDelay = 59.6;
+samplingRate = 256; %256 Hz for the Emotiv headset
 
 %Get paths to your .csv files using cmd/terminal
-pathEmotiv = "/Users/ethan/Desktop/Code/EMC/emc-training/EthanData/Psychopy_Ethan Nguyen_2023-02-08T161045.915958_EPOCX_160085_2023.02.08T16.10.45.08.00.csv";
-pathPsycho = "/Users/ethan/Desktop/Code/EMC/emc-training/EthanData/Ethan Nguyen_23-02-08_16-10-26.csv";
+path = "/Users/ethan/Desktop/Code/EMC/emc-training/EthanData/Psychopy_Ethan Nguyen_2023-02-08T161045.915958_EPOCX_160085_2023.02.08T16.10.45.08.00.csv";
 
 %Reading In Data
-emotivData = readtable(pathEmotiv);
-psychoData = readtable(pathPsycho);
+Data = readtable(path);
 
-keyPressed = table2array(psychoData(:,2));
-reactionTimes = table2array(Data(:,3))*1000 - timeDelay;
-correctness = table2array(Data(:,4));
+O1Data = [table2array(Data(:, 1)) table2array(Data(:, 11)) table2array(Data(:, 23))];
+eegData = zeros(28, 3 * samplingRate);
 
-redStimuli = [];
-blueStimuli = [];
-
-i = 1;
-while i <= 50
-    if correctness(i) == "True"
-        if keyPressed == "f"
-            redStimuli(end + 1) = reactionTimes(i);
-        elseif keyPressed == "j"
-            blueStimuli(end + 1) = reactionTimes(i);
-        end
-    elseif correctness(i) == "False"
-        if keyPressed == "f"
-            blueStimuli(end + 1) == reactionTimes(i);
-        elseif keyPressed == "j"
-            redStimuli(end + 1) = reactionTimes(i);
-        end
+trialCounter = 1;
+for i = 1:length(O1Data) - 256
+    if O1Data(i,3) == 1
+        eegData(trialCounter,:) = O1Data(i - samplingRate:i + 2 * samplingRate - 1, 2);
+        trialCounter = trialCounter + 1;
     end
-    i = i + 1;
 end
 
-%% Plotting Red Color
-
-figure()
-hold on
-
-%Changing data type and specifying row containing RTs
-avals = rmoutliers(redStimuli,'quartiles');
-
-%Plot avals in a histogram and toggle bar & edge color
-histogram(avals,'FaceColor','#EE0000', 'EdgeColor', 'w', 'BinWidth',20);
-
-mn = mean(avals,'omitnan');
-stdv = std(avals,'omitnan');
-
-namelabel = 'Red Stimuli';
-mnlabel = sprintf('Mean: %4.2f ms', mn);
-stdlabel = sprintf('Std Deviation: %4.2f ms', stdv);
-
-n = annotation('textbox',[0.62 0.65 0.1 0.1]);
-
-set(n,'String',{namelabel, mnlabel,stdlabel});
-
-%% Plotting Blue Color
-
-avals = rmoutliers(blueStimuli,'quartiles');
-
-histogram(avals,'FaceColor', '#0000EE', 'EdgeColor', 'w', 'BinWidth',20);
-
-mn = mean(avals,'omitnan');
-stdv = std(avals,'omitnan');
-
-namelabel = 'Blue Stimuli';
-mnlabel = sprintf('Mean: %4.2f ms', mn);
-stdlabel = sprintf('Std Deviation: %4.2f ms', stdv);
-
-n = annotation('textbox',[0.62 0.50 0.1 0.1]);
-
-set(n,'String',{namelabel, mnlabel,stdlabel});
-
-%%
-legend('Red Stimulus', 'Blue Stimulus');
-axis([0 600 0 20])
-%[ymin ymax xmin xmax]
-
-xlabel("Reaction Time (ms)");
-ylabel("Frequency");
-
-title("Reaction Time Distribution for 2 Decision-Making between Red and Blue Stimulus");
+% mean()
+averagedEEGData = mean(eegData, 1);
+writematrix(averagedEEGData','EEGAnalysisAveraged.csv') 
